dd92874cd46d3571391de704c885c90ae5126a35
Astrie-num <uwasterie07@gmail.com>
2025-02-02 21:51:21 +0200

    Decoded data matrix

--- Decoding/matrix.py
+++ Decoding/matrix.py
@@ -5,17 +5,17 @@ import cv2
 image = cv2.imread("image2.jpg", cv2.IMREAD_GRAYSCALE) 
 
 
-decoded_objects = decode(image)
+decoded = decode(image)
 
-for obj in decoded_objects:
+for obj in decoded:
     data = obj.data.decode("utf-8")
-    print("Decoded Data:", data)
+    print("Decoded information:", data)
 
 
 output_file = "decoded_datamatrix.png"
 cv2.imwrite(output_file, image)
-print(f" Annotated image saved as {output_file}")
+print(f" Image saved as {output_file}")
 
-cv2.imshow("Barcode with Annotation", image)
+cv2.imshow("Barcode", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
