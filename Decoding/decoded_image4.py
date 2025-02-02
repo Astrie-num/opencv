3099dc4cd4a8b5484565d32c4e7f98dac6de8c82
Astrie-num <uwasterie07@gmail.com>
2025-02-02 22:01:27 +0200

    Decoded the code on image 4

--- Decoding/decoded_image4.py
+++ Decoding/decoded_image4.py
@@ -1,10 +1,10 @@
 from pyzbar.pyzbar import decode
 from PIL import Image
 
-image_path = "image4.jpg"  # Change this to your file path
+image_path = "image4.jpg" 
 
 image = Image.open(image_path)
-decoded_objects = decode(image)
+decoded = decode(image)
 
-for obj in decoded_objects:
+for obj in decoded:
     print("Decoded Data:", obj.data.decode("utf-8"))
